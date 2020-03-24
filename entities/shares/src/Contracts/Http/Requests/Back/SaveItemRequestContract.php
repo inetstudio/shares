5a6ed8709360d3131a56647170a1ff62d8916d32
Konstantin Bukin <bukin@inetstudio.ru>
2020-03-24 19:33:54 +0300

    Update to 7.0

--- entities/shares/src/Contracts/Http/Requests/Back/SaveItemRequestContract.php
+++ entities/shares/src/Contracts/Http/Requests/Back/SaveItemRequestContract.php
@@ -1,12 +1,9 @@
 <?php
 
 namespace InetStudio\SharesPackage\Shares\Contracts\Http\Requests\Back;
-
-use InetStudio\AdminPanel\Base\Contracts\Http\Requests\BaseRequestContract;
-
 /**
  * Interface SaveItemRequestContract.
  */
-interface SaveItemRequestContract extends BaseRequestContract
+interface SaveItemRequestContract
 {
 }
