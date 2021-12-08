811521023742ea0d81e9e44d1a6a62db565b09cd
Konstantin Bukin <bukin@inetstudio.ru>
2021-12-08 15:02:57 +0300

    Update

--- entities/shares/resources/views/back/includes/navigation.blade.php
+++ entities/shares/resources/views/back/includes/navigation.blade.php
@@ -1,3 +1,3 @@
-<li class="{{ isActiveRoute('back.shares-package.shares.*') }}">
+<li class="{{ isActiveRoute('back.shares-package.shares.*', 'mm-active') }}">
     <a href="{{ route('back.shares-package.shares.index') }}"><i class="fa fa-share"></i> <span class="nav-label">Shares</span></a>
 </li>
